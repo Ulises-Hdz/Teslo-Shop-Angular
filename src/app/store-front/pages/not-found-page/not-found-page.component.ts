18485c01865a8e71943c881ba7dbfa219474a28c
Ulises <dogzack12@gmail.com>
2026-07-18 20:38:25 -0600

    Paginas y rutas listas para mostrar contenido

---
+++ src/app/store-front/pages/not-found-page/not-found-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [],
+  templateUrl: './not-found-page.component.html',
+})
+export class NotFoundPageComponent {}
