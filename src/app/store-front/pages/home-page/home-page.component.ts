8e0356a873f1971a52102b6d6b2c99ef2ee1f58d
Ulises <dogzack12@gmail.com>
2026-07-26 18:07:31 -0600

    Implementar componente de tarjeta de producto con datos dinámicos y agregar servicio de producto

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductCardComponent } from '../../../products/components/product-card/product-card.componets';
+import { ProductsService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Product } from '@products/interfaces/product.interface';
 
 @Component({
   selector: 'app-home-page',
   imports: [ ProductCardComponent ],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  products = signal<Product[]>([])
+  productService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({params}) =>{
+      return this.productService.getProducts({})
+    },
+  });
+
+}
